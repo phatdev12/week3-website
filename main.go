ef36e7dcc77255183a737540a021fa9afa42f722
phatdev <tuphat3456tt@gmail.com>
2025-09-26 16:08:45 +0700

    feat: update

--- main.go
+++ main.go
@@ -28,6 +28,15 @@ func main() {
 		})
 	})
 
+	r.GET("/category", func(c *gin.Context) {
+		var products []models.Product
+		database.DB.Find(&products)
+
+		c.HTML(200, "category.tmpl", gin.H{
+			"products": products,
+		})
+	})
+
 	r.GET("/category/:id", func(c *gin.Context) {
 		var products []models.Product
 		database.DB.Where("id_category = ?", c.Param("id")).Find(&products)
@@ -36,5 +45,27 @@ func main() {
 			"products": products,
 		})
 	})
+
+	r.GET("/create-product", func(c *gin.Context) {
+		var categories []models.Category
+		database.DB.Find(&categories)
+
+		c.HTML(200, "createProduct.tmpl", gin.H{
+			"categories": categories,
+		})
+	})
+
+	r.POST("/products", func(c *gin.Context) {
+		var product models.Product
+
+		if err := c.ShouldBind(&product); err != nil {
+			c.String(400, "Bad request")
+			return
+		}
+
+		database.DB.Create(&product)
+		c.Redirect(302, "/")
+	})
+
 	r.Run(":3000")
 }
--- models/models.go
+++ models/models.go
@@ -14,5 +14,5 @@ type Product struct {
 	Name       string  `gorm:"type:varchar(100);not null"`
 	Price      float64 `gorm:"type:decimal(20,2);not null"`
 	Quantity   int     `gorm:"not null"`
-	IDCategory uint    `gorm:"not null"`
+	IDCategory uint    `gorm:"not null" json:"id_category" form:"id_category"`
 }
